88609cc95a71631b800a495a582a8d44022ce292
Lucas Vieira <lucas_samuel2005@hotmail.com>
2015-08-09 23:22:36 -0300

    Added more features

--- CGOL/main.cpp
+++ CGOL/main.cpp
@@ -22,6 +22,8 @@ typedef unsigned char byte;
 
 float SQUARE_SIDE_N;
 bool pause_required = true;
+int fps = 5;
+int TRUEWIDTH, TRUEHEIGHT;
 
 constexpr byte n_squares() { return WIDTH / SQUARE_SIDE; }
 
@@ -34,8 +36,55 @@ void OnKeyPress(unsigned char, int, int);
 void OnMouseClick(int, int, int, int);
 void OnMouseMove(int, int);
 
+long long int lifespan = 0l;
+char lifespan_string[30];
+
+void renderBitmapString(float x, float y, char *string)
+{
+    glRasterPos2f(x, y);
+	char* c;
+    for (c = string; *c != '\0'; c++)
+    {
+        if((*c) == '\n')
+        {
+            y -= 0.045;
+            glRasterPos2f(x, y);
+        }
+        else glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, *c);
+    }
+}
+
+
 bool** createTable();
 void   deleteTable(bool**);
+
+
+// Tools for creating specific figures
+
+// Static figures
+void createBlock(int x, int y, int angle);
+void createBeehive(int, int, int);
+void createLoaf(int, int, int);
+void createBoat(int, int, int);
+
+// Oscillators
+void createBlinker(int, int, int);
+void createToad(int, int, int);
+void createBeacon(int, int, int);
+void createPulsar(int, int, int);
+void createPentadecathlon(int, int, int);
+
+// Spaceships
+void createGlider(int, int, int);
+void createLWSS(int, int, int);
+
+// Misc
+void createGosperGliderGun(int, int, int);
+void createRPentomino(int, int, int);
+
+char instructions[] = "Press ] to start iterations, [ to stop\n"
+                      "Press > to advance frame by frame\n"
+                      "Pushing ] more than once will make the game go faster";
 
 struct neighbor
 {
@@ -53,11 +102,14 @@ struct neighbor
 
 int main(int argc, char** argv)
 {
+	TRUEWIDTH = WIDTH;
+	TRUEHEIGHT = HEIGHT;
+
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
 	glutInitWindowPosition(0, 0);
 	glutInitWindowSize(WIDTH, HEIGHT);
-	glutCreateWindow("Game of Life");
+	glutCreateWindow("Conway's Game of Life");
 	glutFullScreen();
 	glutDisplayFunc(OnDisplay);
 	glutReshapeFunc(OnReshape);
@@ -69,39 +121,23 @@ int main(int argc, char** argv)
 
 	SQUARE_SIDE_N = (2.0f / n_squares());
 
-	// Initialize?
-	//table[0][0] = true;
-	//table[1][0] = true;
-	//table[0][1] = true;
-
-	// Glider 1
-	table[10][10] = true;
-	table[11][10] = true;
-	table[12][10] = true;
-	table[10][11] = true;
-	table[11][12] = true;
-
-	// Glider 2
-	table[0][1] = true;
-	table[1][2] = true;
-	table[2][0] = true;
-	table[2][1] = true;
-	table[2][2] = true;
+	// Glider
+	//createGlider(0, 0, 0);
 
 	// Lightweight spaceship 1
-	table[0][15] = true;
-	table[3][15] = true;
-	table[4][16] = true;
-	table[0][17] = true;
-	table[4][17] = true;
-	table[1][18] = true;
-	table[2][18] = true;
-	table[3][18] = true;
-	table[4][18] = true;
+	//createLWSS(0, 0, 0);
+
+	//createGosperGliderGun(0, 0, 0);
+
+	//createPentadecathlon(0, 30, 0);
+	//createBlinker(0, 50, 0);
+	//createToad(0, 55, 0);
+	//createBeacon(0, 62, 0);
+	//createPulsar(20, 30, 0);
+	createRPentomino(n_squares() / 2, n_squares() / 2, 0);
 
 	glClearColor(0.8f, 0.8f, 0.8f, 1.0f);
 
-	//OnIdle(0);
 	glutMainLoop();
 
 	deleteTable(table);
@@ -169,24 +205,44 @@ void OnIdle(int val)
 		}
 	deleteTable(table);
 	table = newTable;
+	lifespan++;
 	glutPostRedisplay();
 	if(!pause_required)
-		glutTimerFunc(1000 / 10, OnIdle, 0);
+		glutTimerFunc(1000 / fps, OnIdle, 0);
 }
 
 void OnDisplay()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
-
-	// Draw board
 	glColor3f(0.0f, 0.0f, 0.0f);
+	// Draw grid
+	for(byte i = 0; i < n_squares(); i++)
+	{
+		glPushMatrix();
+		glBegin(GL_LINES);
+		glVertex2f(-1.0f + (i * SQUARE_SIDE_N), 1.0f);
+		glVertex2f(-1.0f + (i * SQUARE_SIDE_N), -1.0f);
+		glEnd();
+		glPopMatrix();
+	}
+	for(byte j = 0; j < n_squares(); j++)
+	{
+		glPushMatrix();
+		glBegin(GL_LINES);
+		glVertex2f(-1.0f, 1.0f - (j * SQUARE_SIDE_N));
+		glVertex2f(1.0f, 1.0f - (j * SQUARE_SIDE_N));
+		glEnd();
+		glPopMatrix();
+	}
+
+	// Draw cells
 	for(byte i = 0; i < n_squares(); i++)
 		for(byte j = 0; j < n_squares(); j++)
 		{
 			if(table[i][j])
 			{
 				// TODO: NORMALIZE SIZES
-				float xpos = -1.0f + (SQUARE_SIDE_N),
+				float xpos = -1.0f,
 				      ypos =  1.0f - (SQUARE_SIDE_N);
 				xpos += (SQUARE_SIDE_N * i);
 				ypos -= (SQUARE_SIDE_N * j);
@@ -202,16 +258,26 @@ void OnDisplay()
 			}
 		}
 
+	if(pause_required) {
+		glColor3f(0.7f, 0.0f, 0.0f);
+		renderBitmapString(-1.0f, 0.9f, instructions);
+	}
+	sprintf(lifespan_string, "Age: %d\nFPS: %d", lifespan, fps);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	renderBitmapString(-1.0f, -0.9f, lifespan_string);
+
 	glutSwapBuffers();
 }
 
 void OnReshape(int w, int h)
 {
+	TRUEWIDTH = w;
+	TRUEHEIGHT = h;
 	glViewport(0, 0, w, h);
 
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	//gluOrtho2D(-1.0f, 1.0f, -1.0f, 1.0f);
+	gluOrtho2D(-1.0f, 1.0f, -1.0f, 1.0f);
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
@@ -222,12 +288,21 @@ void OnReshape(int w, int h)
 void OnKeyPress(unsigned char key, int x, int y)
 {
 	if(key == ']') {
-		pause_required = false;
-		OnIdle(0);
+		if(pause_required) {
+			pause_required = false;
+			OnIdle(0);
+		}
+		else
+		{
+			fps += 5;
+		}
 	}
 	else if(key == '[')
+	{
 		pause_required = true;
-	else if(key == '.' && pause_required) OnIdle(0);
+		fps = 5;
+	}
+	else if(key == '>' && pause_required) OnIdle(0);
 }
 
 bool mousedown = false;
@@ -235,15 +310,21 @@ void OnMouseClick(int button, int state, int x, int y)
 {
 	if(button == GLUT_LEFT_BUTTON)
 	{
-		glutPostRedisplay();
+		float xpos_perc, ypos_perc;
+		xpos_perc = float(x) / float(TRUEWIDTH);
+		ypos_perc = float(y) / float(TRUEHEIGHT);
+
 		if(state == GLUT_DOWN)
 		{
 			if(x <= WIDTH && y <= HEIGHT)
-				table[x / SQUARE_SIDE][y / SQUARE_SIDE] = true;
+				//table[x / SQUARE_SIDE][y / SQUARE_SIDE] = true;
+				table[int(xpos_perc * n_squares())][int(ypos_perc * n_squares())] = true;
 			mousedown = true;
 		}
 		else
 			mousedown = false;
+
+		glutPostRedisplay();
 	}
 }
 
@@ -272,4 +353,257 @@ void   deleteTable(bool** oldtable)
 	for(byte b = 0; b < n_squares(); b++)
 		delete [] oldtable[b];
 	delete [] oldtable;
+}
+
+
+
+
+
+
+//==========================
+// I'm disconsidering angle for now until I find a
+// proper way to rotate the actual figure around its axis.
+
+void createBlock(int x, int y, int angle)
+{
+	table    [x][y]     = true;
+	table[x + 1][y]     = true;
+	table    [x][y + 1] = true;
+	table[x + 1][y + 1] = true;
+}
+
+void createBeehive(int x, int y, int angle)
+{
+	table[x + 1][y]     = true;
+	table[x + 2][y]     = true;
+	table    [x][y + 1] = true;
+	table[x + 3][y + 1] = true;
+	table[x + 1][y + 2] = true;
+	table[x + 2][y + 2] = true;
+}
+
+void createLoaf(int x, int y, int angle)
+{
+	table[x + 1][y]     = true;
+	table[x + 2][y]     = true;
+	table    [x][y + 1] = true;
+	table[x + 3][y + 1] = true;
+	table[x + 1][y + 2] = true;
+	table[x + 3][y + 2] = true;
+	table[x + 2][y + 3] = true;
+}
+
+void createBoat(int x, int y, int angle)
+{
+	table    [x][y]     = true;
+	table[x + 1][y]     = true;
+	table    [x][y + 1] = true;
+	table[x + 2][y + 1] = true;
+	table[x + 1][y + 2] = true;
+}
+
+void createBlinker(int x, int y, int angle)
+{
+	table[x + 1][y + 2] = true;
+	table[x + 2][y + 2] = true;
+	table[x + 3][y + 2] = true;
+}
+
+void createToad(int x, int y, int angle)
+{
+	table[x + 2][y + 2] = true;
+	table[x + 3][y + 2] = true;
+	table[x + 4][y + 2] = true;
+
+	table[x + 1][y + 3] = true;
+	table[x + 2][y + 3] = true;
+	table[x + 3][y + 3] = true;
+}
+
+void createBeacon(int x, int y, int angle)
+{
+	createBlock(x + 1, y + 1, angle);
+	createBlock(x + 3, y + 3, angle);
+}
+
+void createPulsar(int x, int y, int angle)
+{
+	table[x + 4][y + 2] = true;
+	table[x + 5][y + 2] = true;
+	table[x + 6][y + 2] = true;
+
+	table[x + 10][y + 2] = true;
+	table[x + 11][y + 2] = true;
+	table[x + 12][y + 2] = true;
+
+
+
+	table[x + 2][y + 4] = true;
+	table[x + 7][y + 4] = true;
+	table[x + 9][y + 4] = true;
+	table[x + 14][y + 4] = true;
+
+	table[x + 2][y + 5] = true;
+	table[x + 7][y + 5] = true;
+	table[x + 9][y + 5] = true;
+	table[x + 14][y + 5] = true;
+
+	table[x + 2][y + 6] = true;
+	table[x + 7][y + 6] = true;
+	table[x + 9][y + 6] = true;
+	table[x + 14][y + 6] = true;
+
+
+
+
+	table[x + 4][y + 7] = true;
+	table[x + 5][y + 7] = true;
+	table[x + 6][y + 7] = true;
+
+	table[x + 10][y + 7] = true;
+	table[x + 11][y + 7] = true;
+	table[x + 12][y + 7] = true;
+
+
+
+
+	table[x + 4][y + 9] = true;
+	table[x + 5][y + 9] = true;
+	table[x + 6][y + 9] = true;
+
+	table[x + 10][y + 9] = true;
+	table[x + 11][y + 9] = true;
+	table[x + 12][y + 9] = true;
+
+
+
+	table[x + 2][y + 10] = true;
+	table[x + 7][y + 10] = true;
+	table[x + 9][y + 10] = true;
+	table[x + 14][y + 10] = true;
+
+	table[x + 2][y + 11] = true;
+	table[x + 7][y + 11] = true;
+	table[x + 9][y + 11] = true;
+	table[x + 14][y + 11] = true;
+
+	table[x + 2][y + 12] = true;
+	table[x + 7][y + 12] = true;
+	table[x + 9][y + 12] = true;
+	table[x + 14][y + 12] = true;
+
+
+
+
+	table[x + 4][y + 14] = true;
+	table[x + 5][y + 14] = true;
+	table[x + 6][y + 14] = true;
+
+	table[x + 10][y + 14] = true;
+	table[x + 11][y + 14] = true;
+	table[x + 12][y + 14] = true;
+}
+
+void createPentadecathlon(int x, int y, int angle)
+{
+
+	                      table[x + 5][y + 1] = true;
+	                      table[x + 5][y + 2] = true;
+
+	                      table[x + 5][y + 4] = true;
+	table[x + 4][y + 5] = true;                   table[x + 6][y + 5] = true;
+
+    table[x + 4][y + 7] = true; table[x + 5][y + 7] = true; table[x + 6][y + 7] = true;
+
+
+    table[x + 4][y + 10] = true; table[x + 5][y + 10] = true; table[x + 6][y + 10] = true;
+
+    table[x + 4][y + 12] = true;                   table[x + 6][y + 12] = true;
+                          table[x + 5][y + 13] = true;
+
+    	                  table[x + 5][y + 15] = true;
+	                      table[x + 5][y + 16] = true;
+}
+
+
+void createGlider(int x, int y, int angle)
+{
+	table    [x][y + 1] = true;
+	table[x + 1][y + 2] = true;
+	table[x + 2][y]     = true;
+	table[x + 2][y + 1] = true;
+	table[x + 2][y + 2] = true;
+}
+
+void createLWSS(int x, int y, int angle)
+{
+	table    [x][y + 15] = true;
+	table[x + 3][y + 15] = true;
+	table[x + 4][y + 16] = true;
+	table    [x][y + 17] = true;
+	table[x + 4][y + 17] = true;
+	table[x + 1][y + 18] = true;
+	table[x + 2][y + 18] = true;
+	table[x + 3][y + 18] = true;
+	table[x + 4][y + 18] = true;
+}
+
+void createGosperGliderGun(int x, int y, int angle)
+{
+	createBlock(x + 1, y + 5, angle);
+
+	table[x + 11][y + 5] = true;
+	table[x + 11][y + 6] = true;
+	table[x + 11][y + 7] = true;
+
+	table[x + 12][y + 4] = true;
+	table[x + 12][y + 8] = true;
+
+	table[x + 13][y + 3] = true;
+	table[x + 13][y + 9] = true;
+
+	table[x + 14][y + 3] = true;
+	table[x + 14][y + 9] = true;
+
+	table[x + 15][y + 6] = true;
+
+	table[x + 16][y + 4] = true;
+	table[x + 16][y + 8] = true;
+
+	table[x + 17][y + 5] = true;
+	table[x + 17][y + 6] = true;
+	table[x + 17][y + 7] = true;
+
+	table[x + 18][y + 6] = true;
+
+
+
+
+	table[x + 21][y + 3] = true;
+	table[x + 21][y + 4] = true;
+	table[x + 21][y + 5] = true;
+
+	table[x + 22][y + 3] = true;
+	table[x + 22][y + 4] = true;
+	table[x + 22][y + 5] = true;
+
+	table[x + 23][y + 2] = true;
+	table[x + 23][y + 6] = true;
+
+
+	table[x + 25][y + 1] = true;
+	table[x + 25][y + 2] = true;
+	table[x + 25][y + 6] = true;
+	table[x + 25][y + 7] = true;
+
+	createBlock(x + 35, y + 3, angle);
+}
+
+void createRPentomino(int x, int y, int angle)
+{
+	table[x + 2][y + 1] = true;
+	table[x + 3][y + 1] = true;
+	table[x + 1][y + 2] = true;
+	table[x + 2][y + 2] = true;
+	table[x + 2][y + 3] = true;
 }
